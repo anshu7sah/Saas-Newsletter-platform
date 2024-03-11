c6e0f0d654cc0e41f5ed702ec82167e004841c73
Anshu Sah <anshu7sah@gmail.com>
2024-03-11 08:44:47 +0545

    completed branding part of homepage

---
+++ src/modules/home/features/branding.tsx
@@ -0,0 +1,35 @@
+import { partners } from "@/app/configs/constants";
+import Image from "next/image";
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const Branding = () => {
+  return (
+    <div className="border-t border-b border-[#000] py-10">
+      <h3 className="uppercase text-xl md:text-2xl text-center max-w-3xl mx-auto font-[400] z-20 relative">
+        CREATED BY THE EARLY MORNING BREW TEAM
+      </h3>
+      <div className="w-full text-center pt-1">
+        <h3 className="uppercase bg-[#F091DD] rounded p-2 text-xl md:text-2xl text-center inline-block font-medium z-20 relative ">
+          NOW POWERING THE WORLD&apos;S TIP NEWSLETTERS
+        </h3>
+      </div>
+      <Marquee className="w-full flex">
+        {partners.map((partner: PartnersTypes, i: number) => (
+          <>
+            <Image
+              src={partner.url}
+              alt="partner"
+              key={i}
+              width={200}
+              height={200}
+              className="md:mx-8 w-[150px] md:w-[180px] mx-3"
+            />
+          </>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default Branding;
--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,12 +1,14 @@
 import Header from "@/shared/widgets/header/header";
 import React from "react";
 import Banner from "./features/banner";
+import Branding from "./features/branding";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Branding />
     </div>
   );
 };
